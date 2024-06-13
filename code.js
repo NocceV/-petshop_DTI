d86c93203d6710347e893e4d1cf0a9399f97e074
Vitor Nocce <noccevitor@gmail.com>
2024-06-13 13:49:28 -0300

    Aplicando Responsividade

--- code.js
+++ code.js
@@ -11,83 +11,88 @@ document.getElementById('formulario').addEventListener('submit',function(event){
 
     const diaSemanal = data.getUTCDay();
     
-    const petShops = [
+    if(qtdCaoGrande >= 0 && qtdCaoPequeno>=0){
+        const petShops = [
 
-        {
-            nome : "Meu Canino Feliz",
-            distanciaCanil : 2000,
-            DogPequenoPreco : 20,
-            DogGrandePreco : 40,
-            DogPequenoFim : 24,
-            DogGrandeFim :  48
-        },
+            {
+                nome : "Meu Canino Feliz",
+                distanciaCanil : 2000,
+                DogPequenoPreco : 20,
+                DogGrandePreco : 40,
+                weekEndAddP: 4,
+                weekEndAddG : 8
+            },
 
-        {
-            nome : "Vai Rex",
-            distanciaCanil : 1700,
-            DogPequenoPreco : 15,
-            DogGrandePreco : 50,
-            DogPequenoFim : 20,
-            DogGrandeFim :  55
-        },
+            {
+                nome : "Vai Rex",
+                distanciaCanil : 1700,
+                DogPequenoPreco : 15,
+                DogGrandePreco : 50,
+                weekEndAddP : 5,
+                weekEndAddG : 5
+            },
 
-        {
-            nome : "ChowChawgas",
-            distanciaCanil : 800,
-            DogPequenoPreco : 30,
-            DogGrandePreco : 45,
-            DogPequenoFim : 30,
-            DogGrandeFim :  45
-        }
+            {
+                nome : "ChowChawgas",
+                distanciaCanil : 800,
+                DogPequenoPreco : 30,
+                DogGrandePreco : 45,
+                weekEndAddP : 0,
+            weekEndAddG :  0
+            }
 
 
-    ];
-      
-    function calcularValorBanho(qtdPequeno, qtdGrande, petShop,dia) {
-        if(dia != 0 && dia != 6){
-        return (qtdPequeno * petShop.DogPequenoPreco) + (qtdGrande * petShop.DogGrandePreco);
-        }
-        else {
-            return (qtdPequeno * petShop.DogPequenoFim) + (qtdGrande * petShop.DogGrandeFim);
-        }
+        ];
         
-    }
-  
-    function menorDistancia(pet1 , pet2){
-
-        if(pet1.distanciaCanil <= pet2.distanciaCanil){
-            return pet1;
-        }
-        else{
-            return pet2;
-        }
-    }  
-    
-    function acharMelhorShop(P,G,petShops,dia){
-   
-        let melhorShop = petShops[0];       
-         for(let i = 1 ;i < petShops.length;i++ ){
-                      
-            const temp1 = calcularValorBanho(P,G,melhorShop,dia);
-            const temp2 = calcularValorBanho(P,G,petShops[i],dia);
+        function calcularValorBanho(qtdPequeno, qtdGrande, petShop,dia) {
+            if(dia != 0 && dia != 6){
+            return (qtdPequeno * petShop.DogPequenoPreco) + (qtdGrande * petShop.DogGrandePreco);
+            }
+            else {
+                return (qtdPequeno *(petShop.DogPequenoPreco+petShop.weekEndAddP)) + (qtdGrande * (petShop.DogGrandePreco+petShop.weekEndAddG));
+            }
             
-            if(temp2 < temp1){
-                melhorShop = petShops[i];
-            }
-            else if(temp1==temp2){
-                melhorShop = menorDistancia(melhorShop,petShops[i]);
-            }
-             
-                
         }
-        return melhorShop;    
-       
+    
+        function menorDistancia(pet1 , pet2){
+
+            if(pet1.distanciaCanil <= pet2.distanciaCanil){
+                return pet1;
+            }
+            else{
+                return pet2;
+            }
+        }  
+        
+        function acharMelhorShop(P,G,petShops,dia){
+    
+            let melhorShop = petShops[0];       
+            for(let i = 1 ;i < petShops.length;i++ ){
+                        
+                const temp1 = calcularValorBanho(P,G,melhorShop,dia);
+                const temp2 = calcularValorBanho(P,G,petShops[i],dia);
+                
+                if(temp2 < temp1){
+                    melhorShop = petShops[i];
+                }
+                else if(temp1==temp2){
+                    melhorShop = menorDistancia(melhorShop,petShops[i]);
+                }
+                
+                    
+            }
+            return melhorShop;    
+        
+        }
+
+        const melhorDeTodos = acharMelhorShop(qtdCaoPequeno,qtdCaoGrande,petShops,diaSemanal);
+        const precoTotal = calcularValorBanho(qtdCaoPequeno,qtdCaoGrande,melhorDeTodos,diaSemanal);
+
+        document.getElementById('escrita').textContent = melhorDeTodos.nome + " R$:"+ precoTotal; 
     }
-
-    const melhorDeTodos = acharMelhorShop(qtdCaoPequeno,qtdCaoGrande,petShops,diaSemanal);
-    const precoTotal = calcularValorBanho(qtdCaoPequeno,qtdCaoGrande,melhorDeTodos,diaSemanal);
-
-    document.getElementById('escrita').textContent = melhorDeTodos.nome + " R$:"+ precoTotal; 
+    else{
+        document.getElementById('escrita').textContent = "Parâmetros não aceitos"; 
+    }
     
 });
 
